afd388890412e073a3b52020cbdff7e0e946c1bf
Alviery026 <143028187+Alviery026@users.noreply.github.com>
2023-12-10 18:20:42 -0600

    Add files via upload

---
+++ Multas.c
@@ -0,0 +1,13 @@
+
+#include "Multas.h"
+
+void AplicarMultas(struct NodoUsuario** usuario, float monto) {
+ 
+}
+
+void VerificarRetraso(struct NodoTransaccion** transaccion, struct NodoUsuario** usuario, float montoMulta) {
+    
+}
+
+
+
